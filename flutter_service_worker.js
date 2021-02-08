85b68b4811fca70ee6209b2e5d825b7087ef9aa5
Wecone <huangwelcone@gmail.com>
2021-02-08 12:55:11 +0800

    1.10.2 解决级别统计bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8ee27545e607dd1c39de08571ca51ab6",
-"index.html": "859b6e521f025992e9235074d3655e48",
-"/": "859b6e521f025992e9235074d3655e48",
-"main.dart.js": "f73c79f8922250f78db7bf0820a9507f",
+"index.html": "33c8958feb13dcaa7e9eae0a3738719b",
+"/": "33c8958feb13dcaa7e9eae0a3738719b",
+"main.dart.js": "a3d95f05f1190b614906709d80a79b2a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
